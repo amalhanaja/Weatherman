b3cf57da9355cca84596fc6831d76b86dd83ef6f
amalhanaja <amalhanaja@gmail.com>
2023-02-28 01:05:49 +0700

    feat: dummy abstraction for getting city

--- feature/home/src/main/java/dev/amalhanaja/weatherman/feature/home/CityListUiState.kt
+++ feature/home/src/main/java/dev/amalhanaja/weatherman/feature/home/CityListUiState.kt
@@ -4,7 +4,6 @@ import dev.amalhanaja.weatherman.core.model.City
 
 sealed interface CityListUiState {
     object Empty : CityListUiState
-    object Loading : CityListUiState
     data class Failed(val message: String) : CityListUiState
     data class WithData(val isFavoriteList: Boolean, val cities: List<City>) : CityListUiState
 }
--- feature/home/src/main/java/dev/amalhanaja/weatherman/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/dev/amalhanaja/weatherman/feature/home/HomeViewModel.kt
@@ -8,8 +8,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.amalhanaja.weatherman.core.model.City
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
@@ -23,9 +25,11 @@ class HomeViewModel @Inject constructor() : ViewModel() {
         private set
 
     val cityListUiState: StateFlow<CityListUiState> = snapshotFlow { searchQuery }
-        .flatMapLatest {
-            flowOf((1..4).map { City("Name $it", null, "ID", 0.0, 0.0) })
-        }.map<List<City>, CityListUiState> { CityListUiState.WithData(true, it) }
+        .flatMapLatest { q ->
+            val isGetFromFavorite = q.isBlank()
+            val citiesFlow = if (isGetFromFavorite) getFavoriteCities() else searchCities(q)
+            citiesFlow.map<List<City>, CityListUiState> { CityListUiState.WithData(isGetFromFavorite, it) }
+        }.catch { emit(CityListUiState.Failed(it.message.orEmpty())) }
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(),
@@ -34,6 +38,14 @@ class HomeViewModel @Inject constructor() : ViewModel() {
 
     fun updateSearchQuery(query: String) {
         searchQuery = query
+    }
+
+    private fun getFavoriteCities(): Flow<List<City>> {
+        return flowOf((1..4).map { City("Name $it", null, "ID", 0.0, 0.0) })
+    }
+
+    private fun searchCities(query: String): Flow<List<City>> {
+        return flowOf((1..20).map { City("Name $query $it", null, "ID", 0.0, 0.0) })
     }
 
 }
