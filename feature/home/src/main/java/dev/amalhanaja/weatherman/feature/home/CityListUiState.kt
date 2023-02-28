b3d7f04661a85c7db0815ca14fd515cc5ec131e2
amalhanaja <amalhanaja@gmail.com>
2023-02-28 11:23:58 +0700

    feat(home): add state for empty and search not found

--- feature/home/src/main/java/dev/amalhanaja/weatherman/feature/home/CityListUiState.kt
+++ feature/home/src/main/java/dev/amalhanaja/weatherman/feature/home/CityListUiState.kt
@@ -4,6 +4,7 @@ import dev.amalhanaja.weatherman.core.model.City
 
 sealed interface CityListUiState {
     object Empty : CityListUiState
+    object NotFound : CityListUiState
     data class Failed(val message: String) : CityListUiState
     data class WithData(val isFavoriteList: Boolean, val cities: List<City>) : CityListUiState
 }
--- feature/home/src/main/java/dev/amalhanaja/weatherman/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/dev/amalhanaja/weatherman/feature/home/HomeViewModel.kt
@@ -14,7 +14,6 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
@@ -31,7 +30,13 @@ class HomeViewModel @Inject constructor(
         .flatMapLatest { q ->
             val isGetFromFavorite = q.isBlank()
             val citiesFlow = if (isGetFromFavorite) getFavoriteCities() else searchCities(q)
-            citiesFlow.map<List<City>, CityListUiState> { CityListUiState.WithData(isGetFromFavorite, it) }
+            citiesFlow.map { cities ->
+                when {
+                    cities.isEmpty() && isGetFromFavorite -> CityListUiState.Empty
+                    cities.isEmpty() -> CityListUiState.NotFound
+                    else -> CityListUiState.WithData(isGetFromFavorite, cities)
+                }
+            }
         }.catch { emit(CityListUiState.Failed(it.message.orEmpty())) }
         .stateIn(
             scope = viewModelScope,
@@ -42,13 +47,8 @@ class HomeViewModel @Inject constructor(
     fun updateSearchQuery(query: String) {
         searchQuery = query
     }
+    private fun getFavoriteCities(): Flow<List<City>> = cityRepository.getFavoriteCities()
 
-    private fun getFavoriteCities(): Flow<List<City>> {
-        return flowOf((1..4).map { City("Name $it", "", "ID", 0.0, 0.0) })
-    }
-
-    private fun searchCities(query: String): Flow<List<City>> {
-        return cityRepository.searchCities(query)
-    }
+    private fun searchCities(query: String): Flow<List<City>> = cityRepository.searchCities(query)
 
 }
